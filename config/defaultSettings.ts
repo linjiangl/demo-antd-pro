933aabea2591abc700b482619baff8e3095a431a
linjiangl <8257796@qq.com>
2025-07-09 17:57:40 +0800

    官方配色

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -20,25 +20,49 @@ const Settings: ProLayoutProps & {
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
   openKeys: false,  // 菜单选中，其他菜单不自动收缩
-  // footerRender: false,
+  footerRender: false,
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
     //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
-    bgLayout: '#FFFFFF',
+    bgLayout: 'linear-gradient(${antdToken.colorBgContainer}, ${antdToken.colorBgLayout} 28%)',
+    colorTextAppListIcon: '	#666',
+    colorTextAppListIconHover: 'rgba(0, 0, 0, 0.65)',
+    colorBgAppListIconHover: 'rgba(0, 0, 0, 0.04)',
     sider: {
-      colorMenuBackground: '#FFFFFF',
-      colorMenuItemDivider: '#DFDFDF',
-      colorTextMenu: '#595959',
-      colorTextMenuSelected: '#2A7AFBFF',
-      colorBgMenuItemSelected: '#E6F3FEFF',
+      colorMenuBackground: "transparent",
+      colorTextMenuTitle: "colorTextHeading",
+      colorMenuItemDivider: "colorSplit",
+      colorTextMenu: "colorText",
+      colorTextMenuSecondary: "colorText",
+      colorTextMenuSelected: "rgb(0,0,0)",
+      colorTextMenuActive: "rgba(0, 0, 0, 0.85)",
+      colorTextMenuItemHover: "rgba(255,255,255,0.75)",
+      colorBgMenuItemActive: "rgba(0, 0, 0, 0.15)",
+      colorBgMenuItemHover: "rgba(90, 75, 75, 0.03)",
+      colorBgMenuItemSelected: "rgba(0, 0, 0, 0.04)",
+      colorBgMenuItemCollapsedElevated: "transparent",
+      colorBgCollapsedButton: "#fff",
+      colorTextCollapsedButton: "colorTextMenuSecondary",
+      colorTextCollapsedButtonHover: "colorTextMenu"
     },
     header: {
-      colorTextRightActionsItem: '#000000F2',
+      colorBgHeader: "rgba(240, 242, 245, 0.4)",
+      colorHeaderTitle: "colorTextHeading",
+      colorTextMenu: "colorText",
+      colorTextMenuSecondary: "colorText",
+      colorTextMenuSelected: "rgb(0,0,0)",
+      colorTextMenuActive: "rgba(0, 0, 0, 0.85)",
+      colorBgMenuItemHover: "rgba(90, 75, 75, 0.03)",
+      colorBgMenuItemSelected: "rgba(0, 0, 0, 0.04)",
+      colorTextRightActionsItem: "colorTextSecondary",
+      colorBgRightActionsItemHover: "rgba(0, 0, 0, 0.03)",
+      heightLayoutHeader: 56
     },
     pageContainer: {
-      colorBgPageContainer: '#F6F9FB',
       paddingBlockPageContainerContent: 24,
-      paddingInlinePageContainerContent: 24
+      paddingInlinePageContainerContent: 40,
+      colorBgPageContainer: "transparent",
+      colorBgPageContainerFixed: "#FFF"
     }
   }
 };
