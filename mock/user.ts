80bec1b7c94278a570675f9d0a7639ee2d2caaf6
linjiangl <8257796@qq.com>
2025-07-11 17:53:56 +0800

    build: upgrade react

--- mock/user.ts
+++ mock/user.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 
 const waitTime = (time: number = 100) => {
   return new Promise((resolve) => {
@@ -8,7 +8,7 @@ const waitTime = (time: number = 100) => {
   });
 };
 
-async function getFakeCaptcha(req: Request, res: Response) {
+async function getFakeCaptcha(_req: Request, res: Response) {
   await waitTime(2000);
   return res.json('captcha-xxx');
 }
@@ -29,7 +29,7 @@ const getAccess = () => {
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 export default {
   // 支持值为 Object 和 Array
-  'GET /api/currentUser': (req: Request, res: Response) => {
+  'GET /api/currentUser': (_req: Request, res: Response) => {
     if (!getAccess()) {
       res.status(401).send({
         data: {
@@ -45,7 +45,8 @@ export default {
       success: true,
       data: {
         name: 'Serati Ma',
-        avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+        avatar:
+          'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
         userid: '00000001',
         email: 'antdesign@alipay.com',
         signature: '海纳百川，有容乃大',
@@ -155,14 +156,14 @@ export default {
     });
     access = 'guest';
   },
-  'POST /api/login/outLogin': (req: Request, res: Response) => {
+  'POST /api/login/outLogin': (_req: Request, res: Response) => {
     access = '';
     res.send({ data: {}, success: true });
   },
-  'POST /api/register': (req: Request, res: Response) => {
+  'POST /api/register': (_req: Request, res: Response) => {
     res.send({ status: 'ok', currentAuthority: 'user', success: true });
   },
-  'GET /api/500': (req: Request, res: Response) => {
+  'GET /api/500': (_req: Request, res: Response) => {
     res.status(500).send({
       timestamp: 1513932555104,
       status: 500,
@@ -171,7 +172,7 @@ export default {
       path: '/base/category/list',
     });
   },
-  'GET /api/404': (req: Request, res: Response) => {
+  'GET /api/404': (_req: Request, res: Response) => {
     res.status(404).send({
       timestamp: 1513932643431,
       status: 404,
@@ -180,7 +181,7 @@ export default {
       path: '/base/category/list/2121212',
     });
   },
-  'GET /api/403': (req: Request, res: Response) => {
+  'GET /api/403': (_req: Request, res: Response) => {
     res.status(403).send({
       timestamp: 1513932555104,
       status: 403,
@@ -189,7 +190,7 @@ export default {
       path: '/base/category/list',
     });
   },
-  'GET /api/401': (req: Request, res: Response) => {
+  'GET /api/401': (_req: Request, res: Response) => {
     res.status(401).send({
       timestamp: 1513932555104,
       status: 401,
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
       style={{
         background: 'none',
       }}
-      copyright={false}
+      copyright="Powered by Ant Desgin"
       links={[
         {
           key: 'Ant Design Pro',
