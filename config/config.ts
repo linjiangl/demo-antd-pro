6830104263a9ea7a66b317b6d1c7199a7d428af3
linjiangl <8257796@qq.com>
2025-07-18 18:16:42 +0800

    ci: config.ts 显示问题修改

--- config/config.ts
+++ config/config.ts
@@ -15,7 +15,7 @@ const { UMI_ENV = 'dev' } = process.env;
  */
 const PUBLIC_PATH: string = '/';
 
-export default defineConfig({
+const config = {
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
@@ -179,4 +179,7 @@ export default defineConfig({
   define: {
     'process.env.CI': process.env.CI,
   },
-});
+};
+
+// @ts-ignore
+export default defineConfig(config) as any;
